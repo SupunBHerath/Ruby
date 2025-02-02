19dd17062ca09fd53cfe46487fe305b324e7a758
SupunBHerath <supunbherath@gmail.com>
2025-02-02 15:38:59 +0530

    if

---
+++ 02/02/if.rb
@@ -0,0 +1,22 @@
+age = 18
+
+if age >= 18
+  puts "you are eligible to vote"
+end
+
+if age > 18
+  puts "you are eligible to vote"
+else
+  puts "you are not eligible to vote"
+end
+
+if age > 18
+  puts "you are eligible to vote"
+
+elsif age > 16 
+  puts "16 -18"
+
+else 
+  puts "under 16"
+
+end
