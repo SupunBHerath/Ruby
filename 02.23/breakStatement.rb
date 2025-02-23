7fdd709bffc6245d031fc820f3833abb261b6a06
SupunBHerath <supunbherath@gmail.com>
2025-02-23 14:12:35 +0530

    break statement Sample

---
+++ 02.23/breakStatement.rb
@@ -0,0 +1,17 @@
+for i in 1..10
+  puts "Current number is #{i}"
+  break if i == 5
+end
+
+puts "\n Loop exited with break !"
+
+
+# ----Output------
+
+# Current number is 1
+# Current number is 2
+# Current number is 3
+# Current number is 4
+# Current number is 5
+
+#  Loop exited with break !
