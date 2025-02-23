b55036ada0714472ffe444b5266bad91b3aa2bcc
SupunBHerath <supunbherath@gmail.com>
2025-02-23 15:48:47 +0530

    update nested loop code

--- 02.23/nestedLoop.rb
+++ 02.23/nestedLoop.rb
@@ -2,6 +2,33 @@
   (1..3).each do |j|
     puts "i : #{i} , j :#{j}"
   end
-end     
+end   
+  
+puts "\n\n"
+
+(1..5).each do |i|
+  (1..i).each do |j|
+    print "* "
+  end
+  puts
+end  
 
 # Equivalent Java Code Using for-each Loop
+
+
+# i : 1 , j :1
+# i : 1 , j :2
+# i : 1 , j :3
+# i : 2 , j :1
+# i : 2 , j :2
+# i : 2 , j :3
+# i : 3 , j :1
+# i : 3 , j :2
+# i : 3 , j :3
+
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
