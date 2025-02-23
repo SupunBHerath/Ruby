4764a952cc4a2a5f6cb0d70c4fc9e40413418426
SupunBHerath <supunbherath@gmail.com>
2025-02-23 13:53:53 +0530

    forLoop Sample

---
+++ 02.23/forLoop.rb
@@ -0,0 +1,29 @@
+# range of number
+
+for i in 1..5
+  puts "Current number is #{i}"
+end
+
+puts "loop finished\n\n"
+
+fruits = ["apple","banana","cherry"]
+
+for fruit in fruits 
+  puts "Current fruit is #{fruit}"
+end
+
+puts "loop finished"
+
+
+# outputs------
+# Current number is 1
+# Current number is 2
+# Current number is 3
+# Current number is 4
+# Current number is 5
+# loop finished
+
+# Current fruit is apple
+# Current fruit is banana
+# Current fruit is cherry
+# loop finished
