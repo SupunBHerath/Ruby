50a14e5de728af05cee431c2cfafc76a67f3ccee
SupunBHerath <supunbherath@gmail.com>
2025-03-09 15:05:03 +0530

    Accessing Element Using `find` Method

---
+++ 03.09/array9.rb
@@ -0,0 +1,6 @@
+array = [10,20,30,40,50,60]
+
+puts "Accessing Element Using `find` Method " 
+puts "First element greater than 50 : #{array.find{|element|element>50}}"
+
+# First element greater than 50 : 60
