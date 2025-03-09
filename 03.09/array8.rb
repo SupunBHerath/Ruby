437e955ad3524e1cc8d528ff64926957d7b47120
SupunBHerath <supunbherath@gmail.com>
2025-03-09 14:59:49 +0530

    Accessing Elements Using the `index` Method

--- 03.09/array8.rb
+++ 03.09/array8.rb
@@ -4,3 +4,6 @@ puts "Accessing Elements Using the `index` Method : "
 puts "Index of element 50 : #{array.index(50)}"
 puts "Index of element 200 (not found) : #{array.index(200) || "Not found"}"
 
+# Accessing Elements Using the index Method : 
+# Index of element 50 : 4
+# Index of element 200 (not found) : Not found
