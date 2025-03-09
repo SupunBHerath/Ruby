ddd16677102c2a8c56eb6b58816f7e06ba64cb8b
SupunBHerath <supunbherath@gmail.com>
2025-03-09 14:31:56 +0530

    Using `each` to print all elements

---
+++ 03.09/array5.rb
@@ -0,0 +1,15 @@
+array = [10,20,30,40,50,60]
+
+puts "Iterating Over the Array : "
+puts "Using `each` to print all elements : "
+array.each{|element| puts element}
+
+
+# Iterating Over the Array : 
+# Using `each` to print all elements :
+# 10
+# 20
+# 30
+# 40
+# 50
+# 60
