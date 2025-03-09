dc02cb459bc36b0d822166e9aef3d383a2e1a08c
SupunBHerath <supunbherath@gmail.com>
2025-03-09 14:26:08 +0530

    Accessing Elements Using the `values_at Method :

---
+++ 03.09/array4.rb
@@ -0,0 +1,7 @@
+array = [10,20,30,40,50,60]
+
+puts "Accessing Elements Using the `values_at Method : "
+puts "Element at indices 1,3 and 5 : #{array.values_at(1,3,5)}" 
+
+# Accessing Elements Using the `values_at Method : 
+# Element at indices 1,3 and 5 : [20, 40, 60]
