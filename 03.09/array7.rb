0e57e99ebffdb4c9af2b798cf7828ae906b3afe3
SupunBHerath <supunbherath@gmail.com>
2025-03-09 14:53:56 +0530

    Accessing Elements Using the sample Method

---
+++ 03.09/array7.rb
@@ -0,0 +1,10 @@
+array = [10,20,30,40,50,60]
+
+puts "Accessing Elements Using the sample Method  : "
+puts "Random element : #{array.sample}"
+puts "Random element : #{array.sample(3).inspect}"
+
+
+# Accessing Elements Using the sample Method  : 
+# Random element : 50
+# Random element : [50, 10, 30]
