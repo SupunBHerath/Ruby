f6df0b2d546d29b0edcb34a18aa07dcffc42e835
SupunBHerath <supunbherath@gmail.com>
2025-03-09 15:15:25 +0530

    Modify Eelement 1

---
+++ 03.09/ModifyElement/arrayM1.rb
@@ -0,0 +1,9 @@
+array = [10,20,30,40,50,60]
+
+puts "Original Array : #{array}"
+
+array[2]=200
+puts "Modified Array : #{array}"
+
+# Original Array : [10, 20, 30, 40, 50, 60]
+# Modified Array : [10, 20, 200, 40, 50, 60]
