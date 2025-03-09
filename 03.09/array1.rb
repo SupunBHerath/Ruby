6cb280ac4c0cfbc6468567026b407fd1a927ecaf
SupunBHerath <supunbherath@gmail.com>
2025-03-09 13:44:54 +0530

    Accessing Elements by Index

---
+++ 03.09/array1.rb
@@ -0,0 +1,13 @@
+# Program 1 : Accessing Elements by Index 
+arry = [10,20,30,40,50,60]
+
+puts "Accessing Elements by Index : "
+puts "First element : #{arry[0]}"
+puts "Third element : #{arry[3]}"
+puts "Last element  : #{arry[-1]}"
+
+# output
+# Accessing Elements by Index : 
+# First element : 10
+# Third element : 40
+# Last element  : 60
